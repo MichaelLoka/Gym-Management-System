56bfea3e62d29d2d6e06fa6f434374ba52b0d7a1
MarioDou7 <mariomamdouhreda@gmail.com>
2020-12-31 01:11:20 +0200

    finshing trainer Class

--- Project/Trainer.java
+++ Project/Trainer.java
@@ -2,15 +2,29 @@ package Project;
 
 public class Trainer extends notAuothority{
 
-    //session[] session;
-    //session[] Holds;
-    //member[] members;
-    //member[] mentor
+    Session[] session;
+    Session[] Holds;
+    Member[] members;
+    Member[] Supervise;
     int Salary;
 
     public Trainer(String Name, int SSN, String Sex, String Address, int PhoneNumber, int Salary){
         super(Name, SSN, Sex, Address, PhoneNumber);
         this.Salary = Salary;
     }
+    public Trainer(String Name, String Sex, String Address, int PhoneNumber, int Salary){
+        super(Name, Sex, Address, PhoneNumber);
+        this.Salary = Salary;
+    }
     
+    public void DisplayData(){
+        System.out.println("Name: "+Name);
+        System.out.println("Sex: "+ Sex);
+        System.out.println("Address: "+Address);
+        System.out.println("Salary: "+Salary);
+        System.out.println("Phone Number:");
+        for(int i = 0 ; i<3;i++){
+            System.out.println(phone[i]);
+        }
+    }
 }
