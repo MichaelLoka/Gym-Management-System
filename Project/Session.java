83deac3bd44c3d6d721fb14dfed21e4107b36bc4
MarioDou7 <mariomamdouhreda@gmail.com>
2020-12-31 01:11:44 +0200

    Adding DisplayData method to Session Class

--- Project/Session.java
+++ Project/Session.java
@@ -21,4 +21,8 @@ public class Session {
             }
         }
     }
+    public void displayData(){
+        System.out.println("description: "+description);
+        System.out.println("type: "+type);
+    }
 }
